936c85ecc5038e11ebf1b0d32708ccf1772e0436
Jose Guadalupe Cortes Cruz <joseegccr@gmail.com>
2022-12-04 13:36:48 -0600

    Creación de proyecto y escenas

---
+++ Assets/MovimientoTopDown.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoTopDown : MonoBehaviour
+{
+    [SerializeField] private float velocidadMovimiento;
+
+    [SerializeField] private Vector2 direccion;
+
+    private Rigidbody2D rb2D;
+
+    private float MovimientoX;
+
+    private float MovimientoY;
+
+    private Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        MovimientoX = Input.GetAxisRaw("Horizontal");
+        MovimientoY = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("MovimientoX", MovimientoX);
+        animator.SetFloat("MovimientoY", MovimientoY);
+
+        if (MovimientoX != 0 || MovimientoY != 0)
+        {
+            animator.SetFloat("UltimoX", MovimientoX);
+            animator.SetFloat("UltimoY", MovimientoY);
+        }
+
+        direccion = new Vector2(MovimientoX, MovimientoY).normalized;
+    }
+
+    private void FixedUpdate() {
+        rb2D.MovePosition(rb2D.position + direccion * velocidadMovimiento * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/BottonCanvas.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottonCanvas : MonoBehaviour
+{
+    public void menu(){
+        Debug.Log("Ir a menu");
+    }
+}
