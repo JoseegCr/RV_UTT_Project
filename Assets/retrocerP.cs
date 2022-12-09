0c256244b41fc4f321aab6204a865bea71226b85
Jose Guadalupe Cortes Cruz <joseegccr@gmail.com>
2022-12-09 03:28:44 -0600

    Info P

---
+++ Assets/retrocerP.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class retrocerP : MonoBehaviour
+{
+   public void retroceder(){
+        SceneManager.LoadScene(5);
+    }
+}
